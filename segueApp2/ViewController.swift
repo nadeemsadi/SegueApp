ccb0cb4b4026fd02bfc436d5d959aa1e4a80eed9
Nadeem Sadiq <nadeemsadiq@nadeems-mac.local>
2016-08-30 00:09:00 -0400

    Initial Commit

---
+++ segueApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  segueApp2
+//
+//  Created by Nadeem Sadiq on 2016-08-30.
+//  Copyright © 2016 Nadeem Sadiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
