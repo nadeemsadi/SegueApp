ac184a7de8a4de662e11e4305dfa26d3417331a2
Nadeem Sadiq <nadeemsadiq@nadeems-mac.local>
2016-08-30 00:20:01 -0400

    Initial Commit

---
+++ Goback.swift
@@ -0,0 +1,13 @@
+//
+//  Goback.swift
+//  segueApp2
+//
+//  Created by Nadeem Sadiq on 2016-08-30.
+//  Copyright © 2016 Nadeem Sadiq. All rights reserved.
+//
+
+import UIKit
+
+class Goback: NSObject {
+
+}
